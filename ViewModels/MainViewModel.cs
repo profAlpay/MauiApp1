28298ebc426e3008f85a8bb22bbc71c372ae2138
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-01-20 10:29:45 +0300

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MauiApp1.Models;
+using MauiApp1.ViewModels.Base;
+
+namespace MauiApp1.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private ObservableCollection<TodoItem> _todoItems;
+        public ObservableCollection<TodoItem> TodoItems
+        {
+            get => _todoItems;
+            set => SetProperty(ref _todoItems, value);
+        }
+
+        private ICommand _addTodoCommand;
+        public ICommand AddTodoCommand => _addTodoCommand ??= new Command(async () => await AddTodoAsync());
+
+        public MainViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>
+            {
+                // Test için örnek veriler
+                new TodoItem { Title = "Alışveriş yapılacak", IsCompleted = false },
+                new TodoItem { Title = "Spor yapılacak", IsCompleted = true },
+                new TodoItem { Title = "Kitap okunacak", IsCompleted = false }
+            };
+        }
+
+        private async Task AddTodoAsync()
+        {
+            string result = await Shell.Current.DisplayPromptAsync("Yeni Görev", "Görev başlığını girin:");
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                TodoItems.Add(new TodoItem { Title = result, IsCompleted = false });
+            }
+        }
+    }
+} 
